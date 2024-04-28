1257571ad1084ebfd87c4453f4e677ed00d4469b
fernando lemos <fernando_lemos@hotmail.com.br>
2024-04-27 22:04:33 -0300

    altered acess port try2

--- Projeto_Notes_FE/src/services/api.js
+++ Projeto_Notes_FE/src/services/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
  // baseURL: "https://rocketseat-api-notes.onrender.com"
-  // baseURL: "http://localhost:3333"
-  baseURL: "http://192.168.100.212/api-rocketnotes"
+  baseURL: "http://localhost:3333"
+  // baseURL: "http://192.168.100.212/api-rocketnotes"
 });
