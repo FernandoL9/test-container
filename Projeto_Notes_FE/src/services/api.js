ba3485b38c10d1901762c2f5d652d03712d7f2e3
fernando lemos <fernando_lemos@hotmail.com.br>
2024-04-29 04:17:32 -0300

    proxy reverse04

--- Projeto_Notes_FE/src/services/api.js
+++ Projeto_Notes_FE/src/services/api.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export const api = axios.create({
   // baseURL: "https://rocketseat-api-notes.onrender.com"
   // baseURL: "http://localhost:3333"
-  baseURL: "http://api-rocketnotes:80/api"
+  baseURL: "http://172.31.25.81:3333/"
 });
